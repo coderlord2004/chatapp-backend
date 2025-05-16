a89ca8467cf0dd989bc84bbbf603e948125638ca
minhnbnt <minhnbnt@gmail.com>
2025-05-16 21:58:20 +0700

    fix: user now can't send invitation if the previous one is pending

--- src/main/java/com/group4/chatapp/repositories/InvitationRepository.java
+++ src/main/java/com/group4/chatapp/repositories/InvitationRepository.java
@@ -1,5 +1,6 @@
 package com.group4.chatapp.repositories;
 
+import com.group4.chatapp.models.ChatRoom;
 import com.group4.chatapp.models.Invitation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,32 @@ public interface InvitationRepository extends JpaRepository<Invitation, Long> {
         where i.receiver.id = ?1
     """)
     Stream<Invitation> findByReceiverId(long id);
+
+    @Query("""
+        select (count(i) > 0) from Invitation i
+        where i.sender.id = ?1
+          and i.receiver.id = ?2
+          and i.chatRoom is null
+          and i.status = ?3
+    """)
+    boolean existsFriendRequestWith(
+        long senderId,
+        long receiverId,
+        Invitation.Status status
+    );
+
+    @Query("""
+        select (count(i) > 0)
+        from Invitation i
+        where i.sender.id = ?1
+          and i.receiver.id = ?2
+          and i.chatRoom.id = ?3
+          and i.status = ?4
+    """)
+    boolean existGroupInvitationWith(
+        long senderId,
+        long receiverId,
+        long chatRoomId,
+        Invitation.Status status
+    );
 }
