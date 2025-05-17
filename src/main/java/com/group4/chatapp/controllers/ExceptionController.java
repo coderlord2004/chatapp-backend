e920f07e8927607bc5254d85b3e7edbd016b5b4f
minhnbnt <minhnbnt@gmail.com>
2025-05-17 23:08:47 +0700

    feat: error now will come with message

---
+++ src/main/java/com/group4/chatapp/controllers/ExceptionController.java
@@ -0,0 +1,7 @@
+package com.group4.chatapp.controllers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ExceptionController extends ResponseEntityExceptionHandler {}
