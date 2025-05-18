d7b58d1c10722c85a50d788ae70037389b0cb457
minhnbnt <minhnbnt@gmail.com>
2025-05-18 10:44:38 +0700

    refactor: some small changes

--- src/main/java/com/group4/chatapp/services/CloudinaryService.java
+++ src/main/java/com/group4/chatapp/services/CloudinaryService.java
@@ -2,9 +2,7 @@ package com.group4.chatapp.services;
 
 import com.cloudinary.Cloudinary;
 import com.group4.chatapp.exceptions.ApiException;
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,19 +12,26 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class CloudinaryService {
+
     private final Cloudinary cloudinary;
 
     public String uploadFile(MultipartFile file, String type) {
         try {
-            Map<String, Object> options = Map.of(
-                    "folder", "Image/",
-                    "resource_type", type
+
+            // TODO: check filetype and choose difference folder
+            var options = Map.of(
+                "folder", "Image/",
+                "resource_type", type
             );
-            return (String) cloudinary.uploader().upload(file.getBytes(), options).get("secure_url");
+
+            return (String) cloudinary.uploader()
+                .upload(file.getBytes(), options)
+                .get("secure_url");
+
         } catch (Exception e) {
             throw new ApiException(
-                    HttpStatus.BAD_REQUEST,
-                    e.getMessage()
+                HttpStatus.BAD_REQUEST,
+                e.getMessage()
             );
         }
     }
