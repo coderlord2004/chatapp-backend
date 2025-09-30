5416ec9bc834b9cb76bfbe2e23b77f641a688e95
coderlord2004 <duyn38520@gmail.com>
2025-10-01 02:33:09 +0700

    remove PublishPostJob in main

--- src/main/java/com/group4/chatapp/ChatApplication.java
+++ src/main/java/com/group4/chatapp/ChatApplication.java
@@ -1,14 +1,10 @@
 package com.group4.chatapp;
 
-import com.group4.chatapp.jobs.PublishPostJob;
 import org.quartz.*;
-import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.time.ZoneId;
-import java.util.Date;
 
 @SpringBootApplication
 @EnableScheduling
