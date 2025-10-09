65ea0f2b9d54937b600f5215dc032e89cc442cd4
coderlord2004 <duyn38520@gmail.com>
2025-10-09 11:34:57 +0700

    add Notification

---
+++ src/main/java/com/group4/chatapp/models/Notification.java
@@ -0,0 +1,19 @@
+package com.group4.chatapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+}
---
+++ src/main/java/com/group4/chatapp/repositories/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.group4.chatapp.repositories;
+
+import com.group4.chatapp.models.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
